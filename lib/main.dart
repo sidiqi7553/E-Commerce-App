c3253f668389415a2fb4283573ad732f292956a4
ahmad-ali3158 <113877531+ahmad-ali3158@users.noreply.github.com>
2023-02-03 03:27:45 -0800

    ok theek ha

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'hoja bhai change hoja'),
+      home: const MyHomePage(title: 'kia masla tha'),
     );
   }
 }
